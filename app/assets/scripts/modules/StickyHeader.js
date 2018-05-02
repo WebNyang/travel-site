88825ce63eea247d3f962c16ed3e2a79c96d44e7
Sangwoo Park <eicpein@gmail.com>
2018-05-02 17:19:38 +0900

    Set offset for upward

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -33,12 +33,28 @@ class StickyHeder {
       var currentPageSection = this;
       new Waypoint({
         element: currentPageSection,
-        handler: function() {
-          var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-          thisClass.headerLinks.removeClass("is-current-link");
+        handler: function(diection) {
+          if (diection == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            thisClass.headerLinks.removeClass("is-current-link");
 
-          $(matchingHeaderLink).addClass("is-current-link");
-        }
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "18%"
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(diection) {
+          if (diection == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            thisClass.headerLinks.removeClass("is-current-link");
+
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "-40%"
       });
     });
   }
