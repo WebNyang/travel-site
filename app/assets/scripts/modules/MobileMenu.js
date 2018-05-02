4c80291ae862af0d27382bd05ebf0b16aad32469
Sangwoo Park <eicpein@gmail.com>
2018-05-02 09:30:07 +0900

    Toggle the menu when the menu button clicked

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,16 +3,17 @@ import $ from 'jquery';
 class MobileMenu {
   constructor() {
     this.menuIcon = $(".site-header__menu-icon");
+    this.menuContent = $(".site-header__menu-content");
 
     this.events();
   }
 
   events() {
-    this.menuIcon.click(this.toggleTheMenu);
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
   }
 
   toggleTheMenu() {
-    console.log("Hooray - the icon was clicked");
+    this.menuContent.toggleClass("site-header__menu-content--is-visible");
   }
 }
 
